8fb3287bfbc56b47e27681ddc56855c32ccb177f
Shatakshi Saha <shatakshi.saha.cse2023@kgec.ac.in>
2026-08-12 18:48:49 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(!isalnum(s[i]))i++;
+            if(!isalnum(s[j]))j--;
+            if(tolower(s[i])!=tolower(s[j])){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
