d000c3152302c3cad5bdec2f23db93e7b1629c03
Shatakshi Saha <shatakshi.saha.cse2023@kgec.ac.in>
2026-09-16 00:23:45 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n= s.size();
+
+        for(int i=0;i<n ;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
+            if(s[i]==')'  ){
+                if(st.empty() || st.top()!='('){
+                    return false;
+                }
+                st.pop();
+            }
+            if(s[i]=='}'  ){
+                if(st.empty() || st.top()!='{'){
+                    return false;
+                }
+                st.pop();
+            }
+            if(s[i]==']'  ){
+                if(st.empty() || st.top()!='['){
+                    return false;
+                }
+                st.pop();
+            }
+            
+        }
+        return st.empty();
+    }
+};
